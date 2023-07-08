e30fabbc6772609bb9d82c5927923300d0999874
Diego Andrade Canosa <diego.andrade@udc.es>
2023-07-08 08:47:51 +0200

    Adjustament

--- pytorch_dist/DDP/002/ddpsrunSBATCH.sh
+++ pytorch_dist/DDP/002/ddpsrunSBATCH.sh
@@ -2,10 +2,10 @@
 # Based on: https://github.com/PrincetonUniversity/multi_gpu_training/tree/main/02_pytorch_ddp
 #SBATCH --job-name=ddp-torch     # create a short name for your job
 #SBATCH --nodes=1               # node count
-#SBATCH --ntasks-per-node=8     # total number of tasks per node
-#SBATCH --cpus-per-task=8       # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --ntasks-per-node=2     # total number of tasks per node
+#SBATCH --cpus-per-task=2       # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem=64G                # total memory per node (4 GB per cpu-core is default)
-#SBATCH --gres=gpu:a100:8             # number of gpus per node
+#SBATCH --gres=gpu:a100:2             # number of gpus per node
 #SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
 
 export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
