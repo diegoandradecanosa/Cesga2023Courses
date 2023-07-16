6cf5c5bb400fbf43da1951fd3a3eb26915c75f58
Diego Andrade Canosa <diego.andrade@udc.es>
2023-07-16 16:21:13 +0200

    Make sure it is deacivated conda env

--- pytorch_dist/scripts/activateconda.sh
+++ pytorch_dist/scripts/activateconda.sh
@@ -3,5 +3,6 @@ module purge
 module load cesga/system miniconda3/22.11
 eval "$(conda shell.bash hook)"
 conda deactivate
+source $STORE/mytorchdist/bin/deactivate
 source $STORE/mytorchdist/bin/activate
 
