bfc4f44ce4489b49c1e4e1a500d8a496aac911eb
Lopez Castro Roberto <roberto.lopez.castro@udc.es>
2023-07-19 00:00:56 +0200

    deep code

---
+++ pytorch_dist/deepspeed/translation/sbatch_deep.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#SBATCH -J jsimple       # Job name
+#SBATCH -o jsimple.o%j   # Name of stdout output file(%j expands to jobId)
+#SBATCH -e jsimple_job.o%j   # Name of stderr output file(%j expands to jobId)
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus=2
+#SBATCH -c 32
+#SBATCH --mem=8G
+#SBATCH -t 00:59:00
+#SBATCH -p short --qos=short
+
+
+echo SLURM_NTASKS: $SLURM_NTASKS
+echo SLURM_CPUS_PER_TASK: $SLURM_CPUS_PER_TASK
+echo SLURM_NNODES: $SLURM_NNODES
+echo SLURM_NTASKS_PER_NODE: $SLURM_NTASKS_PER_NODE
+
+PS=1
+WORKERS=$((SLURM_NTASKS-PS))
+
+
+srun deepspeed --num_gpus 2 test-t5-base.py
